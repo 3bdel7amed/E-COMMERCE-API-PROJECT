64c63e27a1498af06b40f630654b4c975c75c6e7
3bdel7amed <a1312004body10@gmail.com>
2024-09-28 13:48:45 +0300

    Data Seeding

--- Infrastructure/Persistence/PersistenseGlobalUsings.cs
+++ Infrastructure/Persistence/PersistenseGlobalUsings.cs
@@ -1,7 +1,16 @@
-﻿global using Microsoft.EntityFrameworkCore;
-global using System;
+﻿global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
+global using System.Text.Json;
 global using Domain.Entities;
+global using Microsoft.EntityFrameworkCore;
+global using Domain.Contracts.IRepos;
+
+
+
+
+
+
+
