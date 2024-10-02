f0aee7c533948b0eb370946499e6fe222d5c0b12
3bdel7amed <a1312004body10@gmail.com>
2024-10-02 19:45:25 +0300

    Specificaions

---
+++ Core/Service/Specifications/ProductSpecifications.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Contracts.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Specifications
+{
+	public class ProductSpecifications : Specifications<Product>
+	{
+		public ProductSpecifications(int id) : base(p => p.Id == id)
+		{
+			AddInclude(p => p.ProductBrand);
+			AddInclude(p => p.ProductType);
+		}
+		public ProductSpecifications() : base(null)
+		{
+			AddInclude(p => p.ProductBrand);
+			AddInclude(p => p.ProductType);
+		}
+	}
+}
