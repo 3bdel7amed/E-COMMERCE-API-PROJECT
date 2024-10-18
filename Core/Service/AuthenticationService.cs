d753ed73e19fb2ef37c8e3c8be6b4a0886f55193
3bdel7amed <a1312004body10@gmail.com>
2024-10-18 12:16:00 +0300

    Generate Token

--- Core/Service/AuthenticationService.cs
+++ Core/Service/AuthenticationService.cs
@@ -1,8 +1,12 @@
 ﻿using Domain.Exceptions;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +21,7 @@ namespace Service
 			var result = await userManager.CheckPasswordAsync(user, loginModel.Password);
 			if (!result) throw new UnAuthorizedException();
 
-			return mapper.Map<UserResultDto>(user) with { Token = "token" };
+			return mapper.Map<UserResultDto>(user) with { Token = await GenerateTokenAsync(user) };
 		}
 
 		public async Task<UserResultDto> RegisterAsync(RegisterResultDto registerModel)
@@ -34,7 +38,37 @@ namespace Service
 			if (!result.Succeeded) throw new ValidationException
 				(result.Errors.Select(e => e.Description).ToList());
 
-			return mapper.Map<UserResultDto>(user) with { Token = "token" };
+			return mapper.Map<UserResultDto>(user) with { Token = await GenerateTokenAsync(user) };
+		}
+
+		private async Task<string> GenerateTokenAsync(User user)
+		{
+
+			var claims = new List<Claim>
+			{
+				new Claim (ClaimTypes.Name,user.UserName),
+				new Claim (ClaimTypes.Email,user.Email),
+			};
+			var roles = await userManager.GetRolesAsync(user);
+			foreach (var item in roles)
+			{
+				claims.Add(new Claim(ClaimTypes.Role, item));
+			}
+
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Ah_Ya_Shamandora_Sabry_Tal_Rody_We_Gawbeny_3a_Elsoaal"));
+
+			var signingCreds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(
+				claims: claims,
+				signingCredentials: signingCreds,
+				audience: "",
+				issuer: "",
+				expires: DateTime.UtcNow.AddDays(30)
+				);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+
 		}
 	}
 }
